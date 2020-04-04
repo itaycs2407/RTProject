66d12430cd91173b031899bc46ae43a5529e8a53
Itay Cohen <itaycs2407@gmail.com>
2020-04-04 14:44:12 +0300

    added LCD func.

--- ProjModeuls/main/main.ino
+++ ProjModeuls/main/main.ino
@@ -1,14 +1,38 @@
-unsigned long time;
+#include <SoftwareSerial.h>
+#include <LiquidCrystal.h>
+// Moudules setup
+
+// LCD settings.
+const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+// SIMCard Moudle
+SoftwareSerial Sim900Serial(2, 3);
+
+// variable declaratio + setup
+
+const int timeToTakeChildrenOutFromTheCar = 3000; // 3 sec
+const String PHONE = "972587600202";
 const int carIsOnPin = 10;
 int ledPin = 13;
-const int takeChildrenOutFromTheCar = 10000; // 3 sec
+
+int toggle = 0;
+int counter = 1;
+char incoming_char = 0;
+unsigned long time;
 int newMillisRead = 0;
 bool answerFromParent = false;
+
+
+
 void setup() {
   // set carIsOn pin
   pinMode(carIsOnPin, INPUT_PULLUP);
   pinMode(ledPin, OUTPUT);
   digitalWrite(ledPin, LOW);
+  //lcd setup
+  lcd.begin(16, 2);
+
+
 
   Serial.begin(9600);
   Serial.println("start of prog");
@@ -20,50 +44,105 @@ void setup() {
     delay(1);
   }
   CarShutdown();
-  Serial.println("return from car shut down");
-  
+  SIMCardInit();
+  SendMessageToScreen("Initialising...", 0, 0);
+
+  delay(1000);
+  lcd.clear();
+  delay(750);
+  SendMessageToScreen("Standby Mode.", 0, 0);
+
+
+
+
   /*
-     give some time to take out the children out from the
-     car after the car was shutdown.
-     time to dely is takeChildrenOutFromTheCar
+    give some time to take out the children out from the
+    car after the car was shutdown.
+    time to dely is takeChildrenOutFromTheCar
   */
 
-  delay(takeChildrenOutFromTheCar);
-  Serial.println("waited time to take children out of the car");
+  delay(timeToTakeChildrenOutFromTheCar);
+  //Serial.println("waited time to take children out of the car");
+  SendMessageToScreen("Detected children ", 0, 0);
+  SendMessageToScreenWithOutClear
+  ("children ", 0, 1);
 
   /*
-     check for child in the seat :
-     if no, continue to exit (end of setup section. answerFromParent = true;
-     if yes, start the logic
+    check for child in the seat :
+    if no, continue to exit (end of setup section. answerFromParent = true;
+    if yes, start the logic
   */
 
   //while (!answerFromParent) {
   /*
-     logic to decide what sms to send
+    logic to decide what sms to send
   */
   // send the sms
 
   /*
-     while loop tho wait for answer from parent
-     if got answer = change answerFromParent state to true
-     else continue
+    while loop tho wait for answer from parent
+    if got answer = change answerFromParent state to true
+    else continue
 
   */
 
   // check for answerFromParent state :
   /*
-     if false do the logic :
-     action to do.
+    if false do the logic :
+    action to do.
   */
 }
 /**
-   massage + leds to say that the children are out from the car
+  massage + leds to say that the children are out from the car
 */
 void CarShutdown() {
   // send massage to screen - the shutdown
-  Serial.println("the car shutdown");
+  Serial.println("the car shutdown   ");
   digitalWrite(ledPin, HIGH);
 
+  SendMessageToScreen("car shutdown !   ", 0, 0);
+  delay(1500);
+
+  SendMessageToScreenWithOutClear("Initialising.   ", 1, 0);
+
+  delay(1500);
+  lcd.clear();
+
+}
+
+void SIMCardInit() {
+
+  // sms moudle setup:
+  Sim900Serial.begin(115200); // the GPRS baud rate
+  delay(500);
+  Sim900Serial.println("AT+IPR=19200");
+  delay(500);
+  
+  SendMessageToScreen("Initialising..",0,0);
+  delay(300);
+  lcd.clear();
+  Sim900Serial.begin(19200); // the GPRS baud rate
+  delay(1000);
+}
+
+void SendMessageToScreen(String msg, int row, int col)
+{
+
+  // clear the screen
+  lcd.clear();
+  //set cursor
+  lcd.setCursor(col, row);
+  // send the message to the screen
+  lcd.println(msg);
+}
+
+void SendMessageToScreenWithOutClear(String msg, int row, int col)
+{
+
+  //set cursor
+  lcd.setCursor(col, row);
+  // send the message to the screen
+  lcd.println(msg);
 }
 void loop() {
 
--- ProjModeuls/sendSmsOnButtonPress/sendSmsOnButtonPress.ino
+++ ProjModeuls/sendSmsOnButtonPress/sendSmsOnButtonPress.ino
@@ -4,12 +4,10 @@
 const int rs = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
-
-
 int buttonState = 0; // variable for reading the pushbutton status
 int toggle = 0;
 int counter = 1;
-char incoming_char=0;
+char incoming_char = 0;
 const String PHONE = "972587600202";
 const int buttonPin = 8; // the number of the pushbutton pin
 const int ledPin = 13;
@@ -17,15 +15,13 @@ SoftwareSerial Sim900Serial(2, 3);
 
 void setup()
 {
-    //lcd setup
-    lcd.begin(16, 2);
-    lcd.setCursor(0, 0);
-    lcd.print("Initialising.");
-    delay(500);
-    lcd.clear();
-    
-    
-    
+  //lcd setup
+  lcd.begin(16, 2);
+  lcd.setCursor(0, 0);
+  lcd.print("Initialising.");
+  delay(500);
+  lcd.clear();
+
   // sms moudle setup:
   Sim900Serial.begin(115200); // the GPRS baud rate
   delay(500);
@@ -37,17 +33,17 @@ void setup()
   Sim900Serial.begin(19200); // the GPRS baud rate
   delay(1000);
   Serial.begin(9600); // the Hardware serial rate
- 
+
   // initialize the LED pin as an output:
   pinMode(ledPin, OUTPUT);
   // initialize the pushbutton pin as an input:
   pinMode(buttonPin, INPUT);
+
   lcd.print("Initialising...");
   delay(1000);
   lcd.clear();
   delay(1000);
   lcd.print("Standby Mode..");
-  
 }
 
 void loop()
@@ -80,19 +76,20 @@ void loop()
   CheckForIncomingSMS();
 }
 
-void CheckForIncomingSMS(){
-    Sim900Serial.print("AT+CNMI=2,2,0,0,0\r"); 
-    delay(500);
-    Serial.print(Sim900Serial.print("AT+CMGR=1\r"));    // AT Command to receive a live SMS 
-    delay(500);
-    Serial.println("changing to recevie mode...");
-    if (Sim900Serial.available() >6 ){
-      SendMessageToScreen(Sim900Serial.available());
-     //  incoming_char=Sim900Serial.read();    
-      // Serial.print(incoming_char);
-    }    
+void CheckForIncomingSMS()
+{
+  Sim900Serial.print("AT+CNMI=2,2,0,0,0\r");
+  delay(500);
+  Serial.print(Sim900Serial.print("AT+CMGR=1\r")); // AT Command to receive a live SMS
+  delay(500);
+  Serial.println("changing to recevie mode...");
+  if (Sim900Serial.available() > 6)
+  {
+    SendMessageToScreen(Sim900Serial.available());
+    //  incoming_char=Sim900Serial.read();
+    // Serial.print(incoming_char);
+  }
 }
-
 
 void SendMessageToScreen(String msg)
 {
@@ -101,7 +98,6 @@ void SendMessageToScreen(String msg)
   // send the message to the screen
   lcd.println(msg);
 }
-
 
 void SendTextMessage(String msg, String phoneNumber)
 {
